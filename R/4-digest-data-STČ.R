50804594b168bd61c113ad4d6cd4d9f8f027bb41
Jindra Lacko <jindra.lacko@gmail.com>
2021-08-13 20:30:47 +0200

    volební okrsky: RUIAN >> RCzechia

--- R/4-digest-data-STČ.R
+++ R/4-digest-data-STČ.R
@@ -11,25 +11,21 @@ stredni_cechy <- RCzechia::obce_body() %>%
 # načíst výsledky, zafiltrovat na STC kraj + vysčítat do 3 stran + zbytku světa
 stc_vysledky <- read_csv2("./data/kzt6p.csv") %>% 
    filter(OBEC %in% stredni_cechy) %>% 
-   mutate(strana = case_when(KSTRANA == 7 ~ "STAN",
+   mutate(ObecKod = as.character(OBEC), # klíče v RCzechia jsou vždy text
+          Cislo = as.character(OKRSEK),
+          strana = case_when(KSTRANA == 7 ~ "STAN",
                              KSTRANA == 50 ~ "ANO",
                              KSTRANA == 33 ~ "ODS",
                              TRUE ~ "zbytek")) %>% 
-   group_by(OBEC, OKRSEK, strana) %>% 
+   group_by(ObecKod, Cislo, strana) %>% 
    summarise(hlasu = sum(POC_HLASU)) %>% 
    pivot_wider(names_from = strana, values_from = hlasu, values_fill = 0) %>% 
    mutate(celkem = STAN + ANO + ODS + zbytek)
 
-# raw verze z ČUZK; rozbalená by byla moc velká pro GitHub
-unzip("./data/20201003_ST_UVOH.xml.zip",
-      exdir = "./data")
-
 # načíst relevantní shapefile + připojit data frame výsledků
-stc_okrsky <- st_read("./data/20201003_ST_UVOH.xml") %>% 
-   st_set_geometry("OriginalniHranice") %>% 
-   st_transform(4326) %>% 
-   inner_join(stc_vysledky, by = c("ObecKod" = "OBEC", "Cislo" = "OKRSEK")) %>% 
+stc_okrsky <- RCzechia::volebni_okrsky() %>% 
+   inner_join(stc_vysledky, by = c("ObecKod", "Cislo")) %>% 
    select(Kod, ANO, ODS, STAN, zbytek, celkem)
 
 # už není potřeba...
-rm(stc_vysledky)
+rm(stc_vysledky) # už je nepotřebujeme....
