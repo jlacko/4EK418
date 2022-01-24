fedc36c903895505f8eb6da188e48c5e9a830192
Jindra Lacko <jindra.lacko@gmail.com>
2022-01-24 07:47:01 +0100

    rovnání prezky 1. den

--- cvičení/1-dožití.R
+++ cvičení/1-dožití.R
@@ -13,7 +13,7 @@ czso::czso_get_table_schema("130140")
 # Naděje dožití v okresech a správních obvodech ORP při narození
 raw_hope <- czso::czso_get_table("130140") %>% 
    filter(casref_do == as.Date("2020-12-31 UTC") & # poslední řez
-             vek_kod == "400000600001000" ) # při narození
+          vek_kod == "400000600001000" ) # při narození
 
 # nápověda:
 # - okresy jsou v RCzechia::okresy()
