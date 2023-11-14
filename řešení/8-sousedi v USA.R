c1b29ce88f019190d7a830749894a4a55ce1c7d5
Jindra Lacko <jindra@jla-data.net>
2023-11-14 16:10:49 +0100

    řešení neřešeného příkladu 8. týdne

--- řešení/8-sousedi v USA.R
+++ řešení/8-sousedi v USA.R
@@ -17,4 +17,31 @@ ggplot() +
   geom_sf(data = karolina) +
   geom_sf(data = subset(karolina, NAME == "Mecklenburg"), fill = "red")
 
+
 # pro porovnání sousedství uvažujte setdiff.nb() + objekt s nejvíce rozdíly má největší hodnotu lengths()
+
+# sousedi královny
+queen <- karolina %>% 
+  poly2nb(queen = T)
+
+# sousedi věže
+rook <- karolina %>% 
+  poly2nb(queen = F)
+
+# rozdíl mezi sousedstvím věže a královny jako číslo (pořadí prvku)
+qr_rozdil <- setdiff.nb(rook, queen) %>% 
+  lengths() %>% 
+  which.max()
+
+# jméno okresu s největším rozdílem
+karolina$NAME[qr_rozdil]
+
+# vizuálně...
+mapview::mapview(karolina[qr_rozdil,])
+
+# sousedi hrabství Mecklenburg:
+# 1) počadí mecklenburgu mezi 100 counties
+sarlota <- which(karolina$NAME == "Mecklenburg")
+
+# 2) jméno sousedů okresu mecklenburg = prvku v pořadí "sarlota" 
+karolina$NAME[rook[sarlota][[1]]]
