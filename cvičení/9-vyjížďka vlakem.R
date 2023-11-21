04d99ca6bd8762152eadd4248262e5ef6cdaae96
Jindra Lacko <jindra@jla-data.net>
2023-11-21 07:58:48 +0100

    + vyjížďka vlakem jako relativní číslo

--- cvičení/9-vyjížďka vlakem.R
+++ cvičení/9-vyjížďka vlakem.R
@@ -6,12 +6,17 @@ library(dplyr)   # datová manipulace
 library(ggplot2) # kreslení
 library(spdep)   # prostorová statistika
 
-# vyjížďka do školy či práce vlakem
+# vyjížďka do školy či práce vlakem - jako absolutní číslo a jako podíl z vyjížděk
 train_commute <- readr::read_csv("data/sldb2021_vyjizdka_vsichni_prostredek_pohlavi.csv",
                             locale = readr::locale(decimal_mark = ",",
                                                    grouping_mark = " ")) %>% 
-  filter(uzemi_cis == 65 & prostredek_txt == "Vlak" & is.na(pohlavi_txt)) %>% 
-  mutate(uzemi_kod = as.character(uzemi_kod)) # klíče v RCzechia jsou stringy, ne čísla
+  filter(uzemi_cis == 65 & is.na(pohlavi_txt)) %>% 
+  group_by(uzemi_kod) %>% 
+  mutate(podil = hodnota / sum(hodnota)) %>% # vyjížďka typu x jako podíl všech vyjížděk v obci
+  ungroup() %>% 
+  filter(prostredek_txt == "Vlak" ) %>% # jen vlak!
+  mutate(uzemi_kod = as.character(uzemi_kod)) %>% # klíče v RCzechia jsou stringy, ne čísla
+  select(uzemi_kod, uzemi_txt, podil, hodnota)
 
 orpecka <- RCzechia::orp_polygony() %>% 
   left_join(train_commute, by = c("KOD_ORP" = "uzemi_kod"))
