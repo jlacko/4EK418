d0f2eb748ef74a37262911c3e4748b34c92a0963
Jindra Lacko <jindra@jla-data.net>
2024-04-26 12:24:18 +0200

    vychovaná interpolace brna

--- cvičení/10-interpolace-brna.R
+++ cvičení/10-interpolace-brna.R
@@ -8,17 +8,20 @@ library(dplyr)
 library(stars)
 
 # loni v Brně...
-brno <- st_read("./data/brno-AQ.gpkg")
+brno_data <- st_read("./data/brno-AQ.gpkg") %>% 
+  st_transform(5514)
 
 # náš cíl - tady stojí Orloj
 svobodak <- tidygeocoder::geo("náměstí Svobody, Brno") %>% 
-  sf::st_as_sf(coords = c("long", "lat"), crs = 4326) 
+  sf::st_as_sf(coords = c("long", "lat"), crs = 4326) %>% 
+  st_transform(5514)
 
 # cele Brno
-brno <- RCzechia::obce_polygony() %>% 
-  filter(NAZ_OBEC == "Brno")
+brno_mesto <- RCzechia::obce_polygony() %>% 
+  filter(NAZ_OBEC == "Brno") %>% 
+  st_transform(5514)
 
 # pixely po Brně
-brno_stars <- brno %>% 
+brno_stars <- brno_mesto %>% 
   st_bbox() %>% 
   st_as_stars(dx = 500)
