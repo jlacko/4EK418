ad747992ea4d5b919096ed07d01486abfe2069b6
Jindra Lacko <jindra@jla-data.net>
2026-04-21 13:52:09 +0200

    řešení problému šarloty, manželky krále Jiřího

---
+++ řešení/8-sousedi v USA.R
@@ -0,0 +1,53 @@
+# úkol: připravit matici vah a sousedství pro hrabství v Severní Karolnině
+# 1) technikou královny
+# 2) technikou věže
+# 3) identifikovat hrabství s největším rozdílem mezi věží a královnou
+# 4) vypsat sousedy hrabství Mecklenburg (jako Šarlota z Meckleburg-Strelitz, manželka krále Jiřího)
+
+library(sf)      # obecná manipulace s prostorovými daty + soubor nc.shp
+library(dplyr)   # manipulace s data frames
+library(spdep)   # prostorová analytika, včetně sousedství
+library(leaflet) # protože interaktivita :)
+
+# soubor s hrabstvími (counties)
+karolina <- st_read(system.file("shape/nc.shp", package="sf")) # included with sf package
+
+# základní orientace
+leaflet() %>% 
+  addProviderTiles("CartoDB.Positron") %>% 
+  # hrabství, co se NEjmenují po manželce krále Jiřího
+  addPolygons(data = subset(karolina, NAME != "Mecklenburg"),
+              label = ~ NAME) %>% 
+  # hrabství, co se jmenují po manželce krále Jiřího
+  addPolygons(data = subset(karolina, NAME == "Mecklenburg"),
+              label = ~ NAME,
+              color = "red") 
+
+# pro porovnání sousedství uvažujte setdiff.nb() + objekt s nejvíce rozdíly má největší hodnotu lengths()
+
+sousedi_kralovny <- karolina %>% 
+  poly2nb()
+
+sousedi_veze <- karolina %>% 
+  poly2nb(queen = F)
+
+rozdily <- setdiff.nb(sousedi_kralovny,
+                      sousedi_veze)
+
+# okres s největšími rozdíly
+karolina$NAME[which.max(lengths(rozdily))]
+
+# kdo je Franklin? proč se liší??
+leaflet() %>% 
+  addProviderTiles("CartoDB.Positron") %>% 
+  addPolygons(data = subset(karolina, NAME != "Franklin"),
+              label = ~ NAME) %>% 
+  addPolygons(data = subset(karolina, NAME == "Franklin"),
+              label = ~ NAME,
+              color = "red") 
+
+# sousedi okresu s hezkým jménem...
+sousedi_mecklenburg <- unlist(sousedi_kralovny[karolina$NAME == "Mecklenburg"])
+
+# jména sousedů
+karolina$NAME[sousedi_mecklenburg]
